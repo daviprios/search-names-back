197239a0c2f516f4409ef66744fa40c4dc8b2133
Mordecai210 <mordecai210@users.noreply.github.com>
2021-02-09 19:07:14 -0300

    show change get to put ;-;

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 
 use App\Http\Controllers\NameController;
 
-$router->get('/show', 'NameController@getNames');
+$router->put('/show', 'NameController@getNames');
 $router->post('/create', 'NameController@createName');
 $router->patch('/update', 'NameController@updateName');
 $router->put('/delete', 'NameController@deleteName');
